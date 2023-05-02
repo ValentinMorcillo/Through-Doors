3f23b1ad0ab3c742593fe0307812ba849b7f0fd6
PabloGoldman <pablo.goldmanarca@gmail.com>
2023-05-02 12:59:52 -0300

    Updated camera movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody rb;
+    private Vector3 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector3(moveHorizontal, 0f, moveVertical).normalized;
+
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
