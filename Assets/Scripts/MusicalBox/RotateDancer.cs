e430fd6aff7580d51e8616d6cbd0cb6b538f3e6b
Ramaaa9461 <rami.guzmi@gmail.com>
2024-02-26 23:34:11 -0300

    Change controls image

--- Assets/Scripts/MusicalBox/RotateDancer.cs
+++ Assets/Scripts/MusicalBox/RotateDancer.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class RotateDancer : MonoBehaviour
 {
     public bool isActive = false;
+    [SerializeField] Vector3 axisToRotate = Vector3.up;
      float rotationSpeed = 300.0f; // Velocidad de rotación en grados por segundo.
 
     private void Update()
@@ -13,7 +14,7 @@ public class RotateDancer : MonoBehaviour
         if (isActive)
         {
             // Girar el objeto a una velocidad constante en Update.
-            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+            transform.Rotate(axisToRotate * rotationSpeed * Time.deltaTime);
         }
     }
 }
